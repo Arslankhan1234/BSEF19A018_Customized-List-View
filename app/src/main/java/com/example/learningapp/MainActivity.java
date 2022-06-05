ddb73e36b0058552c54f6a14aaa5e8d386a9cf96
Arslan Khan <bsef19a018@pucit.edu.pk>
2022-06-05 11:04:56 -0700

    Leaning Activity

--- app/src/main/java/com/example/learningapp/MainActivity.java
+++ app/src/main/java/com/example/learningapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.learningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,10 +16,21 @@ Button btn1,btn2,btn3;
         setContentView(R.layout.activity_main);
 
         btn1=findViewById(R.id.button1);
+        btn2=findViewById(R.id.button2);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Uri webpage = Uri.parse("https://github.com/Arslankhan1234");
+                Intent intent = new
+                        Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(intent);
+            }
+        });
 
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,Learning.class);
             }
         });
     }
