78bab4e9698ad3301447f5bfd10ef4a61c8aef12
Arslan Khan <bsef19a018@pucit.edu.pk>
2022-06-05 11:06:35 -0700

    ListView in Learning Activity

--- app/src/main/java/com/example/learningapp/Learning.java
+++ app/src/main/java/com/example/learningapp/Learning.java
@@ -3,12 +3,15 @@ package com.example.learningapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class Learning extends AppCompatActivity {
-
+ListView li;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning);
+        li=findViewById(R.id.listview);
+
     }
 }
