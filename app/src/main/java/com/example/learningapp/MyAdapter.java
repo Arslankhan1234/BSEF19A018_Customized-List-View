1517138c65f71f298b8f43f2196a2f8adb2f7e08
Arslan Khan <bsef19a018@pucit.edu.pk>
2022-06-05 11:22:29 -0700

    GetView method in MyAdapter will set text and image

--- app/src/main/java/com/example/learningapp/MyAdapter.java
+++ app/src/main/java/com/example/learningapp/MyAdapter.java
@@ -1,7 +1,12 @@
 package com.example.learningapp;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MyAdapter extends ArrayAdapter {
 
@@ -10,10 +15,20 @@ public class MyAdapter extends ArrayAdapter {
     private  final  int [] images;
 
     public MyAdapter(Activity context,String[] heading,int []images){
-        super(context,R.layout.activity_learning,heading);
+        super(context,R.layout.activity_result,heading);
 
         this.context=context;
         this.heading=heading;
         this.images=images;
     }
+
+    public View getView(int position, View view, ViewGroup parent){
+
+        LayoutInflater inflater=context.getLayoutInflater();
+        View Single=inflater.inflate(R.layout.activity_result,null,true);
+        TextView v=Single.findViewById(R.id.textView);
+        ImageView img=Single.findViewById(R.id.imageview);
+        v.setText(heading[position]);
+        img.setImageResource(images[position]);
+        return Single;
 }
